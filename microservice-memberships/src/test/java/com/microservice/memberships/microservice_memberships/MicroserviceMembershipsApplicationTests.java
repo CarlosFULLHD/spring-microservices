19e3f04b9345f3f7ef8d22c0948c29cad9fd5017
CarlosFULLHD <cninareynaga@gmail.com>
2024-06-03 08:52:07 -0400

    Feat: New microservices init

---
+++ microservice-memberships/src/test/java/com/microservice/memberships/microservice_memberships/MicroserviceMembershipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.memberships.microservice_memberships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMembershipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservice-reservations/src/main/java/com/microservice/reservations/microservice_reservations/MicroserviceReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.reservations.microservice_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceReservationsApplication.class, args);
+	}
+
+}
